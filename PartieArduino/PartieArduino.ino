a2015689200beccc535ce9d1313832b33902c446
Hicham SADDEK <h.saddek@serrahub.com>
2021-06-01 22:37:57 +0200

    Update PartieArduino.ino

--- PartieArduino/PartieArduino.ino
+++ PartieArduino/PartieArduino.ino
@@ -1,64 +1,30 @@
-#include <Wire.h>
-
-#include <SoftwareSerial.h>
-
-#include <Wire.h>
-#include <rgb_lcd.h>  // Faut telecharger le fichier zip ici: https://github.com/Seeed-Studio/Grove_LCD_RGB_Backlight
-#include <math.h>
-
-rgb_lcd LCD;
-
-typedef struct Color{
-  int R = 255;
-  int G = 255;
-  int B = 255;
-} Color;
-
-Color white;
+#include <String>
 
 void setup() {
-  LCD.begin(16, 2);
-  LCD.setRGB(white.R, white.G, white.B);
   Serial.begin(9600);
-  
+}
+
+float get_temperature(){
+  return map(analogRead(1), 0, 1024, 0, 40);
+}
+
+float get_sound(){
+  return map(analogRead(3), 0, 1024, 0, 40);
+}
+
+float get_light(){
+  return map(analogRead(0), 0, 1024, 0, 40);
+}
+
+float get_moisture(){
+  return map(analogRead(4), 0, 1024, 0, 100);
 }
 
 void loop() {
-  // Get Analog values
-  // Get light value
-  int light = analogRead(0);
-  float lux = exp(0.007251*light);
-
-  // Get Temprerature Value
-  int tempValue = analogRead(1);
-  float temperature = map(tempValue, 0, 1024, 0, 40);
-  Serial.print("temp: ");
-  Serial.println(temperature);
-  //Get sound value
-  int sound = analogRead(3);
-  float db = 10 * log10(10/6) + 20 * log10(sound);
-  Serial.print("sound: ");
-  Serial.println(sound);
-
-  // Get humidity
-  int moisture = analogRead(4);
-  float Moist = map(moisture, 0, 1024 , 0 , 100);
-//  Serial.print("moisture value: ");
-//  Serial.println(Moist);
-
-  // Show temperature
-  LCD.setCursor(0, 0);
-  LCD.print("Temp C: ");
-  LCD.print(temperature);
-//
-//  // Show light value
-//  LCD.setCursor(0, 1);
-//  LCD.print("Lumens: ");
-//  LCD.print(lux);
-//  delay(100);
-
-//  //show sound value 
-//  LCD.setCursor(0, 1);
-//  LCD.print("Son db: ");
-//  LCD.print(db);
+  String whatDoUWant = "";
+  if(Serial.available()) whatDoUWant = Serial.readString();
+  if(whatDoUWant == "Temperature") Serial.println(get_temperature());
+  if(whatDoUWant == "Sound") Serial.println(get_sound());
+  if(whatDoUWant == "Light") Serial.println(get_light());
+  if(whatDoUWant == "Moisture") Serial.println(get_moisture());
 }
